0a3e0c5ffc7b73ea985588748e899d2aed413925
axmadJ3 <axmabdukarimov0304@outlook.com>
2024-11-04 22:16:19 +0500

    fix search

--- apps/products/utils.py
+++ apps/products/utils.py
@@ -1,5 +1,17 @@
+from django.db.models import Q
+
 from .models import Products
 
 def query_search(query):
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
+    
+    keywords = [word for word in query.split() if len(word) > 2]
+    
+    query_objects = Q()
+    
+    for keyword in keywords:
+        query_objects |= Q(name__icontains=keyword)
+        query_objects |= Q(description__icontains=keyword)
+        
+    return Products.objects.filter(query_objects)

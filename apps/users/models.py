97a36da5742532ec5c8d5254e988e3cba5f932a3
axmadJ3 <axmabdukarimov0304@outlook.com>
2024-11-16 21:05:10 +0500

    fix user model, and change settings

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    image = models.ImageField(upload_to="users_image", blank=True, null=True, verbose_name='Аватар') 
+    
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'Ползователь'   
+        verbose_name_plural = 'Ползователи'   
+        
+    def __str__(self):
+        return self.username 
+    

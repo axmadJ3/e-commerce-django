6a387054b204ed135d8e2f8913b6fcb298bf96ea
axmadJ3 <axmabdukarimov0304@outlook.com>
2024-11-17 22:53:47 +0500

    added login form

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse
 
+from .forms import UserLoginForm
 
 def login(request):
-    context = {
+    if request.method == 'POST':
+        form = UserLoginForm()
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)       
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
         
+    context = {
+        'title': 'HOME - Авторизация',
+        'form': form,
     }
     
     return render(request, template_name='users/login.html', context=context)

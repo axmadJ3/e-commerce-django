068fe45d729e7ff6c756dfa6f61207b6c872b20b
axmadJ3 <axmabdukarimov0304@outlook.com>
2024-11-18 00:20:04 +0500

    fix base and other templates, complete login form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import AuthenticationForm
 
 from .models import User
 
-class UserLoginForm(AuthenticationForm):
+class UserLoginForm(AuthenticationForm):    
     class Meta:
         model = User
-        
+        fields = ['username', 'password']
--- apps/users/views.py
+++ apps/users/views.py
@@ -7,14 +7,14 @@ from .forms import UserLoginForm
 
 def login(request):
     if request.method == 'POST':
-        form = UserLoginForm()
+        form = UserLoginForm(data=request.POST)
         if form.is_valid():
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)       
-                return HttpResponseRedirect(reverse('index'))
+                return HttpResponseRedirect(reverse('main:home'))
     else:
         form = UserLoginForm()
         

d73160590d2a843502c4831d15163197c0baa57a
axmadJ3 <axmabdukarimov0304@outlook.com>
2024-12-24 21:10:51 +0500

    cart add controller added

--- apps/carts/views.py
+++ apps/carts/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from apps.carts.models import Cart
+from apps.products.models import Products
 
 def cart_add(request, product_slug):
-    pass
+    product = Products.objects.get(slug=product_slug)
+    
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user, product=product).first()
+        
+        if carts.exists():
+            cart.quantity += 1
+            cart.save()
+    else:
+        cart = Cart.object.create(user=request.user, product=product, quantity=1)
+    
+    return redirect(request.META['HTTP_REFERER'])
+
 
 def cart_change(request, product_slug):
     pass

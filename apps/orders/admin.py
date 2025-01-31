155414aa31683a5495d8a06f2bacdfec275d02b2
axmadJ3 <axmabdukarimov0304@outlook.com>
2025-01-31 21:07:02 +0500

    create order is ready

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -2,5 +2,35 @@ from django.contrib import admin
 
 from .models import Order, OrderItem
 
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+class OrderTabular(admin.TabularInline):
+    model = Order
+    fields = ['requires_delivery', 'status', 'payment_on_get', 'is_paid', 'created_timestamp']
+    readonly_fields = ['created_timestamp']
+    search_fields = ['requires_delivery', 'status', 'is_paid', 'created_timestamp']
+    extra = 0
+    
+    
+class OrderItemTabular(admin.TabularInline):
+    model = OrderItem
+    fields = ['product', 'name', 'price', 'quantity']
+    search_fields = ['product', 'name']
+    extra = 0
+    
+    
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'requires_delivery', 'status', 
+                    'payment_on_get', 'is_paid', 'created_timestamp'
+    ]
+    search_fields = ('id',)
+    readonly_fields = ['created_timestamp']
+    list_filter = ['requires_delivery', 'status', 'payment_on_get', 'is_paid', 'created_timestamp']
+    inlines = [OrderItemTabular]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'name', 'price', 'quantity']
+    search_fields = ['order', 'product', 'name']
+    
--- apps/users/admin.py
+++ apps/users/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 
 from .models import User
 from apps.carts.admin import CartTabAdmin
+from apps.orders.admin import OrderTabular
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'first_name', 'last_name', 'email')
     search_fields = ('username', 'first_name', 'last_name', 'email')
-    inlines = [CartTabAdmin]
+    inlines = [CartTabAdmin, OrderTabular]
     
